90d435e81e29dc117f2f46fdea6e2d8cffa3f47d
eltonlauwangchi <61117626+eltonlauwangchi@users.noreply.github.com>
2021-04-14 04:35:23 +0800

    edit on createpost with incomplete sql

--- js/AnswerPost.js
+++ js/AnswerPost.js
@@ -1,3 +1,7 @@
+
+
+
+
 function processform() {
     /*check if user input sth in the comment box, otherwise show alert*/
     var c = document.querySelector("#new-comment").value;
@@ -21,3 +25,65 @@ function processform() {
     newComment.querySelector("small").innerHTML = dateTime;
     document.getElementById('new-comment').value="";
 }
+
+function createpost() {
+    /*check if user input sth in the comment box, otherwise show alert*/
+    var d1 = document.querySelector("#qname").value;
+    if (d1 == "")
+        alert("Please don't leave the Question Box blank");
+	var d2 = document.querySelector("#hashtag").value;
+    if (d2 == "")
+        alert("Please don't leave the Hashtag Box blank");
+	var d3 = document.querySelector("#content").value;
+    if (d3 == "")
+        alert("Please don't leave the Content Box blank");
+	var d4 = document.querySelector("#credit").value;
+    if (d4 == "")
+        alert("Please don't leave the Credit Box blank");
+	
+	/*execute the connect prep, not clear for config*/	
+	let mysql  = require('mysql');
+let config = require('./project1.js');
+let connection = mysql.createConnection(config);
+	/*insert the task to sql*/
+	var sql="INSERT INTO QUESTION VALUES( 0, uid, '$student', '$d2', class, '$d1', '$d3', '$d4', DEFAULT, NULL)";
+/*execute the insert statment*/
+connection.query(sql);
+
+connection.end();
+
+}
+
+function createtask() {
+    /*check if user input sth in the comment box, otherwise show alert*/
+    var d1 = document.querySelector("#qname").value;
+    if (d1 == "")
+        alert("Please don't leave the Question Box blank");
+	var d2 = document.querySelector("#hashtag").value;
+    if (d2 == "")
+        alert("Please don't leave the Hashtag Box blank");
+	var d3 = document.querySelector("#content").value;
+    if (d3 == "")
+        alert("Please don't leave the Content Box blank");
+	var d4 = document.querySelector("#credit").value;
+    if (d4 == "")
+        alert("Please don't leave the Credit Box blank");
+	
+	
+/*execute the connect prep, not clear for config*/	
+	let mysql  = require('mysql');
+let config = require('./project1.js');
+let connection = mysql.createConnection(config);
+
+/*insert the task to sql*/
+	var sql="INSERT INTO QUESTION VALUES( 0, uid, '$teacher', '$d2', class, '$d1', '$d3', '$d4', DEFAULT, NULL)";
+/*execute the insert statment*/
+connection.query(sql);
+
+connection.end();
+
+
+}
+
+
+
