f73549c5143719a0386cd74a26cbea768d523c17
DannyAu <78895522+DannyAu@users.noreply.github.com>
2021-04-14 15:44:22 +0800

    Update AnswerPost.js

--- js/AnswerPost.js
+++ js/AnswerPost.js
@@ -6,16 +6,13 @@ function processform() {
         alert("Please don't leave the Comment Box blank");
         return
     }
-    /*get user information*/
     let lastComment = document.querySelector("#comments").lastElementChild;
     let newComment = document.createElement("div");
     let element = '<div><h5></h5><p></p><small></small></div><hr>';
     newComment.innerHTML = element;
     newComment.className = "d-flex";
     newComment.id = 'c' + (Number(lastComment.id.substr(1)) + 1);
-    /*output username and id*/
     newComment.querySelector("p").innerHTML = document.querySelector("#new-comment").value;
-    /*output user icon*/
     document.querySelector("#comments").appendChild(newComment);
     var today = new Date();
     var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
