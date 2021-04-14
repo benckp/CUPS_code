1d8fdede8dc65dc0480c0385cc84a2a56df19a5b
benckp <74444735+benckp@users.noreply.github.com>
2021-04-14 21:02:01 +0800

    Update forum.js

--- js/forum.js
+++ js/forum.js
@@ -97,84 +97,64 @@ function search() {
 
 // function to sort the table according to user's click
 function sortTable(n) {
-    var table,
-        rows,
-        switching,
+    var list,
         i,
-        x,
-        y,
-        shouldSwitch,
         dir,
-        switchcount = 0;
-    table = document.getElementById("forumList");
-    switching = true;
-    //Set the sorting direction to ascending:
+        rows,
+        swaping,
+        doSwap,
+        row_current,
+        row_next,
+        count = 0;
+    list = document.getElementById("forumList");
+    swaping = true;
     dir = "asc";
-    /*Make a loop that will continue until
-    no switching has been done:*/
-    while (switching) {
-        //start by saying: no switching is done:
-        switching = false;
-        rows = table.getElementsByTagName("TR");
-        /*Loop through all table rows (except the
-        first, which contains table headers):*/
-        for (i = 1; i < rows.length - 1; i++) { //Change i=0 if you have the header th a separate table.
-            //start by saying there should be no switching:
-            shouldSwitch = false;
-            /*Get the two elements you want to compare,
-            one from current row and one from the next:*/
-            x = rows[i].getElementsByTagName("TD")[n];
-            y = rows[i + 1].getElementsByTagName("TD")[n];
-            /*check if the two rows should switch place,
-            based on the direction, asc or desc:*/
+    while (swaping) {
+        swaping = false;
+        rows = list.getElementsByTagName("TR");
+        for (i = 1; i < rows.length - 1; i++) {
+            doSwap = false;
+            row_current = rows[i].getElementsByTagName("TD")[n];
+            row_next = rows[i + 1].getElementsByTagName("TD")[n];
             if (dir == "asc") {
                 if (n == 0) {
-                    if (parseInt(x.innerHTML) > parseInt(y.innerHTML)) {
-                        //if so, mark as a switch and break the loop:
-                        shouldSwitch = true;
+                    if (parseInt(row_current.innerHTML) > parseInt(row_next.innerHTML)) {
+                        doSwap = true;
                         break;
                     }
                 } else {
-                    if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-                        //if so, mark as a switch and break the loop:
-                        shouldSwitch = true;
+                    if (row_current.innerHTML.toLowerCase() > row_next.innerHTML.toLowerCase()) {
+                        doSwap = true;
                         break;
                     }
                 }
             } else if (dir == "desc") {
                 if (n == 0) {
-                    if (parseInt(x.innerHTML) < parseInt(y.innerHTML)) {
-                        //if so, mark as a switch and break the loop:
-                        shouldSwitch = true;
+                    if (parseInt(row_current.innerHTML) < parseInt(row_next.innerHTML)) {
+                        doSwap = true;
                         break;
                     }
                 } else {
-                    if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-                        //if so, mark as a switch and break the loop:
-                        shouldSwitch = true;
+                    if (row_current.innerHTML.toLowerCase() < row_next.innerHTML.toLowerCase()) {
+                        doSwap = true;
                         break;
                     }
                 }
             }
         }
-        if (shouldSwitch) {
-            /*If a switch has been marked, make the switch
-            and mark that a switch has been done:*/
+        if (doSwap) {
             rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-            switching = true;
-            //Each time a switch is done, increase this count by 1:
-            switchcount++;
+            swaping = true;
+            count++;
         } else {
-            /*If no switching has been done AND the direction is "asc",
-            set the direction to "desc" and run the while loop again.*/
-            if (switchcount == 0 && dir == "asc") {
+            if (count == 0 && dir == "asc") {
                 dir = "desc";
-                switching = true;
+                swaping = true;
             }
         }
     }
     for (i = 0; i < 3; i++) {
-        element = table.getElementsByTagName("TR")[0].getElementsByTagName("th")[i];
+        element = list.getElementsByTagName("TR")[0].getElementsByTagName("th")[i];
         if (i != n) {
             element.innerHTML = element.innerHTML.slice(0, -1) + '&#x21F3';
         } else if (dir == "asc") {
